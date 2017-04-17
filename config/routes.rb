2de50f9b7c6ce15bfbcef6f3e8d82d1ea34adc15
Cuong Doan <trongcuong1710@gmail.com>
2017-04-17 18:30:32 +0700

    Fix conflict routes definitions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'authenticates#index'
   post '/login', to: 'authenticates#login'
-  get '/books', to: 'books#index'
+  get '/books/new', to: 'books#new'
+  get '/books(/:page)', to: 'books#index'
   get '/logout', to: 'authenticates#logout'
+  post '/books/create', to: 'books#create'
 end
